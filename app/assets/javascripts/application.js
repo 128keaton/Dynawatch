e60cbe48d601a5e1ad7773567810f13e9cec5853
Keaton Burleson <keaton.burleson@me.com>
2017-12-10 02:27:39 -0600

    updates for full screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,14 @@
 //= require turbolinks
 //= require_tree .
 
-ocument.body.requestFullscreen();
 
-$(document).ready(
-    function() {
-     document.body.requestFullscreen();
+$(document).ready(function(){
+        // iOS web app full screen hacks.
+        if(window.navigator.standalone == true) {
+                // make all link remain in web app mode.
+                $('a').click(function() {
+                        window.location = $(this).attr('href');
+            return false;
+                });
+        }
 });
